6a9d3878ba0c25f0de18eb3d31bb458152c453ab
md imran <imran784@gmail.comls[200~git config --global user.name Imrangit config --global user.email imran784@gmail.comgit config --global user.email aamairconfused@gmail.com>
2026-08-27 03:48:07 +0530

    first version of my code

---
+++ PythonProject1/exception.py
@@ -0,0 +1,7 @@
+if_name_
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    if_name_
+NameError: name 'if_name_' is not defined. Did you mean: '__name__'?
+if_name_==
+SyntaxError: invalid syntax
