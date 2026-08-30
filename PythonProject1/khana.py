7c13f776a9edf328df92f88a30ac8ebc40fb9c99
md imran <imran784@gmail.comls[200~git config --global user.name Imrangit config --global user.email imran784@gmail.comgit config --global user.email aamairconfused@gmail.com>
2026-08-30 05:55:55 +0530

    water

---
+++ PythonProject1/khana.py
@@ -0,0 +1,9 @@
+hungry=input("are u hungry hungry")
+if hungry=="yes":
+    print("eat samosa")
+    print("pijja")
+    print("burger")
+else:
+    thirsty=input("are u thirsty")
+    if thirsty=="yes":
+        print("drink water")
